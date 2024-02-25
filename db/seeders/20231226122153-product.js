5b95ef26659e24c53acd9c7bd9a102817ff485ad
aakghim <114895536+aakghim@users.noreply.github.com>
2024-02-25 17:03:52 +0700

    Update 20231226122153-product.js

--- db/seeders/20231226122153-product.js
+++ db/seeders/20231226122153-product.js
@@ -39,7 +39,7 @@ module.exports = {
           id: uuid(),
           kodebarang: "3",
           namabarang: "Sofa L",
-          image: "https://i.ibb.co/BKwFxFz/Untitled-design-1.png",
+          image: "https://i.ibb.co/hcg0SXJ/Untitled-design-2.png",
           image2: "",
           image3: "",
           typebarang: "Sofa",
